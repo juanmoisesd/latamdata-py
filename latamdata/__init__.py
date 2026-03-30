bd9ee76db14695dce6530fe3b97546837c7920f2
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-30 18:53:06 +0200

    fix: update license to CC0-1.0 and version to 1.0.0

--- latamdata/__init__.py
+++ latamdata/__init__.py
@@ -1,37 +1,19 @@
 """
-latamdata-py: Python package for one-line access to 38 open research datasets from Latin America.
+latamdata-py: One-line access to 38+ open research datasets from Latin America.
 
 Author: Juan Moisés de la Serna Tuya
-ORCID: 0000-0002-8401-8018
-License: CC BY 4.0
+ORCID: https://orcid.org/0000-0002-8401-8018
+Affiliation: Universidad Internacional de La Rioja (UNIR)
+License: CC0 1.0 Universal
 GitHub: https://github.com/juanmoisesd/latamdata-py
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __author__ = "Juan Moisés de la Serna Tuya"
-__license__ = "CC BY 4.0"
+__email__ = "jmsernatuya@gmail.com"
+__license__ = "CC0-1.0"
 __orcid__ = "0000-0002-8401-8018"
 
-from .datasets import (
-    load,
-    load_many,
-    list_all,
-    info,
-    search,
-    load_by_topic,
-    export
-)
+from .datasets import load, list_datasets, info
 
-# Convenience aliases
-datasets = load
-
-__all__ = [
-    "load",
-    "load_many",
-    "list_all",
-    "info",
-    "search",
-    "load_by_topic",
-    "export",
-    "__version__",
-]
+__all__ = ["load", "list_datasets", "info", "__version__"]
